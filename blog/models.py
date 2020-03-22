7ba347c62944eaddc5f17b4eb6b6ab92619239cb
fancychamps <rahulsharmanoj@gmail.com>
2020-03-22 22:48:22 +0530

    From Local-Blog Start

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Blog(models.Model):
+	title = models.CharField(max_length=100)
+	date = models.DateField(auto_now=True)
+	front_image = models.CharField(max_length=100)
+	slug = models.CharField(max_length=120, default="test")
+	description = models.CharField(max_length=500)
+	html_file = models.CharField(max_length=25)
+	public = models.BooleanField()
+
+	def __str__(self):
+		return self.title
+
+	def save(self, *args, **kwargs):
+		self.slug = self.title.replace(' ', '-')
+		super(Blog, self).save(*args, **kwargs)
