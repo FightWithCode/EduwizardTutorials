1a5aadf7f954fbaf92f03a54f5224d5ce95b0e74
fancychamps <rahulsharmanoj@gmail.com>
2020-06-12 08:23:28 +0530

    From Local-Modified URL for Notes

--- notes/views.py
+++ notes/views.py
@@ -47,6 +47,7 @@ def ClassNotesAllSubjectView(request, std):
 
 
 def NotesDownloadView(request, pdfurl):
+	pdfurl = pdfurl + '.pdf'
 	obj = get_object_or_404(Note, notes_document__iexact=pdfurl)
 	notes_obj = Note.objects.filter(note_class__iexact=obj.note_class)[:5]
 	return render(request, 'notes/class_notes_download.html', {'pdfurl': pdfurl, 'obj': obj, 'notes_obj': notes_obj})
