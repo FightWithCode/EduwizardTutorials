9fe09bcc1e30eed261b437a03e19a077d75f60f9
fancychamps <rahulsharmanoj@gmail.com>
2020-06-12 09:42:43 +0530

    Fixed Notes URLs Issue

--- notes/urls.py
+++ notes/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('class/<str:std>/<str:subject>/', views.ClassNotesView, name="ClassNotesView"),
+    path('class/<str:std>/<str:subject>/notes', views.ClassNotesView, name="ClassNotesView"),
     url(r'^class/(?P<std>[\w]+)/available_subjects/$', views.ClassNotesAllSubjectView, name="ClassNotesAllSubjectView"),
     # url(r'^download/(?P<slug>[\w/.-]+)/$', views.NotesDownloadView, name="NotesDownloadView"),
     path('download/<str:slug>/', views.NotesDownloadView, name='NotesDownloadView'),
